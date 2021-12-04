4a8f9bab2cdc1e803caaa2e60af56aef9fffe5f0
notc0rx <73403849+notc0rx@users.noreply.github.com>
2021-12-04 19:16:19 +0100

    Update

--- server-data/resources/[Cores]/ls-core/shared.lua
+++ server-data/resources/[Cores]/ls-core/shared.lua
@@ -45,115 +45,454 @@ Shared.StarterItems = {
 
 Shared.Gangs = {
 	["none"] = {
-		label = "Geen",
+		label = "No Gang Affiliaton",
+		grades = { 
+			[1] = { 
+				label = "None",  
+			}, 
+		} 
 	},
 	["biker"] = {
-		label = "Motor Club",
+		label = "Lost MC",
+		grades = { 
+			[1] = { 
+				label = "Member", 
+			}, 
+			[2] = { 
+				label = "Leader", 
+				boss = true,
+			},
+		} 
 	},
 	["famiglia"] = {
 		label = "La Famiglia",
+		grades = { 
+			[1] = { 
+				label = "Member", 
+			}, 
+			[2] = { 
+				label = "Leader", 
+				boss = true,
+			}, 
+		} 
 	},
 }
 
 Shared.Jobs = {
 	["unemployed"] = {
-		label = "Werkloos",
-		payment = 15,
+		label = "Unemployed",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Unemployed",
+				payment = 100,
+			},
+		}
 	},
-	["taxi"] = {
-		label = "Taxi Chauffeur",
-		payment = 60,
-		defaultDuty = true,
+	["taxi"] = { 
+		label = "Taxi", 
+		defaultDuty = true, 
+		grades = { 
+			[1] = { 
+				label = "Driver", 
+				payment = 200, 
+			}, 
+		} 
 	},
-	["tow"] = {
-		label = "Bergnet",
-		payment = 50,
-		defaultDuty = false,
+	["tow"] = { 
+		label = "Tow", 
+		defaultDuty = false, 
+		grades = { 
+			[1] = { 
+				label = "Driver", 
+				payment = 200, 
+			},
+		} 
+	},
+	["gopostal"] = { 
+		label = "GoPostal", 
+		defaultDuty = false, 
+		grades = { 
+			[1] = { 
+				label = "Delivery Driver", 
+				payment = 200, 
+			},
+		} 
 	},
 	["reporter"] = {
-		label = "Verslag Gever",
-		payment = 60,
+		label = " Los Santos News",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Internshipr",
+				payment = 200,
+			},
+			[2] = {
+				label = "Technician",
+				payment = 200,
+			},
+			[3] = {
+				label = "Reporter",
+				payment = 200,
+			},
+			[4] = {
+				label = "Editor",
+				payment = 200,
+			},
+			[5] = {
+				label = "Board Member",
+				payment = 200,
+			},
+			[6] = {
+				label = "Director",
+				payment = 200,
+				boss = true,
+			},
+		}
 	},
-	["garbage"] = {
-		label = "Vuilnis Medewerker",
-		payment = 50,
-		defaultDuty = false,
+	["garbage"] = { 
+		label = "Garbage Collector", 
+		defaultDuty = true, 
+		grades = { 
+			[1] = { 
+				label = "Driver", 
+				payment = 200, 
+			},
+		} 
+	},
+	["trucker"] = { 
+		label = "Cumbo", 
+		defaultDuty = true, 
+		grades = { 
+			[1] = { 
+				label = "Delivery Driver", 
+				payment = 200, 
+			}, 
+		} 
+	},
+	["takeaway"] = {
+		label = "Takeaway",
+		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Delivery Driver",
+				payment = 200,
+			},
+		}
 	},
 	["burger"] = {
-		label = "Burgershot Medewerker",
-		payment = 60,
+		label = "Burgershot",
 		defaultDuty = false,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+				label = "Shift Supervisor",
+				payment = 200,
+			},
+			[3] = {
+				label = "Supervisor",
+				boss = true,
+				payment = 200,
+			},
+			[4] = {
+				label = "Boss",
+				boss = true,
+				payment = 200,
+			},
+		}
 	},
 	["police"] = {
-		label = "Politie",
-		payment = 250,
+		label = "LSPD",
 		defaultDuty = true,
+		grades = {
+            [1] = {
+                label = "Cadet",
+                payment = 50,
+            },
+            [2] = {
+                label = "Officer",
+                payment = 75,
+            },
+            [3] = {
+                label = "Senior Officer",
+                payment = 100,
+            },
+            [4] = {
+                label = "Sergeant",
+                payment = 125,
+            },
+			[5] = {
+                label = "Head Sergeant",
+                payment = 150,
+            },
+            [6] = {
+                label = "Lieutenant",
+                payment = 175,
+            },
+            [7] = {
+				label = "Assistant Chief",
+				boss = true,
+                payment = 200,
+            },
+			[8] = {
+				label = "Chief",
+				boss = true,
+                payment = 200,
+            },
+		},
 	},
 	["ambulance"] = {
-		label = "Ambulance",
-		payment = 250,
+		label = "EMS",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+                label = "Trainee",
+                payment = 50,
+            },
+            [2] = {
+                label = "EMT",
+                payment = 75,
+            },
+			[3] = {
+                label = "Paramedic",
+                payment = 100,
+            },
+			[4] = {
+                label = "Paramedic FTO",
+                payment = 125,
+            },
+			[5] = {
+                label = "Lieutenant",
+                payment = 150,
+            },
+			[6] = {
+                label = "Captian",
+				boss = true,
+                payment = 175,
+            },
+			[7] = {
+                label = "Assistant Chief",
+				boss = true,
+                payment = 200,
+            },
+            [8] = {
+                label = "Chief",
+				boss = true,
+                payment = 200,
+            },
+        }
 	},
 	["mechanic"] = {
-		label = "Voertuig Monteur",
-		payment = 60,
+		label = "Benny's",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Shift Supervisor",
+				isboss = true,
+                payment = 200,
+			},
+			[3] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
+	},
+	["tuners"] = {
+		label = "Heat Tuners",
+		defaultDuty = true,
+		grades = {
+            [1] = {
+                label = "Employee",
+                payment = 50,
+			},
+			[2] = {
+                label = "Shift Supervisor",
+				isboss = true,
+                payment = 200,
+			},
+			[3] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		},
 	},
 	["repairshop"] = {
-		label = "Voertuig Monteur",
-		payment = 60,
+		label = "Hayes",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Shift Supervisor",
+				isboss = true,
+                payment = 200,
+			},
+			[3] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
+	},
+	["electronic"] = {
+		label = "LSDWP",
+		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Electrician",
+				payment = 200,
+			},
+		}
 	},
 	["judge"] = {
-		label = "Rechter",
-		payment = 350,
+		label = "DOJ",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Judge",
+				payment = 350,
+			},
+		}
 	},
 	["lawyer"] = {
-		label = "Advocaat",
-		payment = 300,
+		label = "Los Santos Lawyers",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
 	},
 	["realestate"] = {
-		label = "Makelaar",
-		payment = 60,
+		label = "Dynasty 8 Realestate",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
 	},
 	["cardealer"] = {
-		label = "Voertuig Verkoper",
-		payment = 60,
+		label = "PDM",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
 	},
 	["stripclub"] = {
-		label = "Stripclub Medewerker",
-		payment = 60,
-		defaultDuty = true,
+		label = "Stripclub",
+		defaultDuty = false,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
+	},
+	["pizza"] = {
+		label = "Pizzeria",
+		defaultDuty = false,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
 	},
 	["winemaker"] = {
-		label = "Wijnmaker",
-		payment = 50,
+		label = "Wine Maker",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
 	},
 	["darkdoctor"] = {
-		label = "Onderwereld Dokter",
-		payment = 60,
+		label = "Underground",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Doctor",
+				payment = 200,
+			},
+		}
 	},
 	["flightschool"] = {
-		label = "Vliegschool",
-		payment = 60,
+		label = "Flightschool",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
 	},
 	["sushi"] = {
-		label = "Sushi Mederwerker",
-		payment = 60,
-		defaultDuty = false,
-	},
-	["motordealer"] = {
-		label = "Motor Dealer",
-		payment = 60,
+		label = "Sushi",
 		defaultDuty = true,
+		grades = {
+			[1] = {
+				label = "Employee",
+				payment = 200,
+			},
+			[2] = {
+                label = "Boss",
+				isboss = true,
+                payment = 250,
+			},
+		}
 	},
 }
